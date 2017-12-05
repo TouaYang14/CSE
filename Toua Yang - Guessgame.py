0cb4349ea768912586c6f69db79d37b6329cecd7
Toua Yang <turtleyang8@gmail.com>
2017-12-05 10:47:30 -0800

    Improvements

--- Toua Yang - Guessgame.py
+++ Toua Yang - Guessgame.py
@@ -1,5 +1,8 @@
 # Toua Yang
 
-print(random.randint(0, 50))
-
-def
+import random
+random_number = (random.randint(1, 50))
+print(random.randint(1, 50))
+def random_number():
+    random_number = input
+    ask = input
--- Warmups.py
+++ Warmups.py
@@ -5,3 +5,13 @@ def reverse_order():
     first_name = input ("First name")
     last_name = input("Last name")
     print("%s, %s" % (last_name, first_name))
+
+
+
+
+
+# 12/5/17
+
+def add_py(name):
+    print("%s.py" % name)   # Solution 1
+    print(name + ".py")     #Solution 2
