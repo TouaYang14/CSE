97f051000f409f8b4b87f38617d7d30361058877
Toua Yang 3195 <sfjg@fresnou.org>
2017-12-08 15:42:26 -0800

    improvements

--- Toua Yang - Guessgame.py
+++ Toua Yang - Guessgame.py
@@ -3,14 +3,52 @@
 import random
 random_number = (random.randint(1, 50))
 
-guesses = 5
+guesses = 4
 
 number = input("What number do you think I am thinking of? ")
+
 
 
 if str(random_number) == number:
     print("You win")
 
+ elif int(guesses) <= random_number:
+    print("Guess Lower")
+
+
 if str(random_number)!= number:
     guesses -= 1
-    print("Take another guess ( ͡° ͜ʖ ͡°)")
+    number = input("Take another guess ( ͡° ͜ʖ ͡°)...")
+
+
+elif int(guesses) <= random_number:
+    print("Guess Lower")
+
+if str(random_number)!= number:
+    guesses -= 1
+    number = input("Take another guess ( ͡° ͜ʖ ͡°)...")
+
+elif int(guesses) <= random_number:
+    print("Guess Lower")
+
+
+if str(random_number)!= number:
+    guesses -= 1
+    number = input("Take another guess ( ͡° ͜ʖ ͡°)...")
+
+
+elif int(guesses) <= random_number:
+    print("Guess Lower")
+
+
+if str(random_number)!= number:
+    guesses -= 1
+    number = input("Take another guess ( ͡° ͜ʖ ͡°)...")
+
+
+elif int(guesses) <= random_number:
+    print("Guess Lower")
+
+
+if guesses == 0:
+    print("You lose")
