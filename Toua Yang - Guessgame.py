ffb21fe90ef0207d4fac5923d09f75fec668c154
Toua Yang <turtleyang8@gmail.com>
2017-12-11 10:48:18 -0800

    cool

--- Toua Yang - Guessgame.py
+++ Toua Yang - Guessgame.py
@@ -5,12 +5,27 @@ random_number = (random.randint(1, 50))
 
 guesses = 5
 
-number = input("What number do you think I am thinking of? ")
+number = -1
+while str(random_number) != number and guesses > 0:
+    number = input("What number do you think I am thinking of? ")
 
+    if str(random_number) == number:
+        print("You win")
 
-if str(random_number) == number:
-    print("You win")
+    if str(random_number) != number:
+        guesses -= 1
 
-if str(random_number)!= number:
-    guesses -= 1
-    print("Take another guess ( ͡° ͜ʖ ͡°)")
+    if number < str(random_number) and guesses > 0:
+        print("Guess Higher")
+        print("Guess again")
+        print("You have %s guesses left." % guesses)
+
+    if number > str(random_number) and guesses > 0:
+        print("Guess Lower")
+        print("Guess again")
+        print("You have %s guesses left." % guesses)
+
+if guesses == 0:
+    print("You lose")
+
+print("You have %s guesses left." % guesses)
