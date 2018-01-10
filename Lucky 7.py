d39117a4707bf4352fe7863c6d39a6e0641593ad
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-01-10 10:45:18 -0800

    Added me stuff

--- Lucky 7.py
+++ Lucky 7.py
@@ -1,3 +1,24 @@
 import random
 
-random.randint(1,12)
+money = 15
+
+rounds = 0
+
+while money > 0:
+    r1 = random.randint(1, 6)
+
+    r2 = random.randint(1, 6)
+
+    if (r1 + r2) != 7:
+        money = money - 1
+        rounds += 1
+        print("You lost the bet. You lost a dollar.")
+        print("You know have $%s." % money)
+
+    if (r1 + r2) == 7:
+        money = money + 4
+        rounds += 1
+
+print("You did %s rounds." % rounds)
+
+print("Your Money is %s dollars." % money)
