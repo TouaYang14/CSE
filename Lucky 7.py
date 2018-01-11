2b71dc458f287eb8508a14d1bd05aeceb36885e7
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-01-11 10:46:52 -0800

    Added print Rounds

--- Lucky 7.py
+++ Lucky 7.py
@@ -13,12 +13,19 @@ while money > 0:
         money = money - 1
         rounds += 1
         print("You lost the bet. You lost a dollar.")
-        print("You know have $%s." % money)
+        print("You now have $%s." % money)
 
     if (r1 + r2) == 7:
         money = money + 4
         rounds += 1
+        print("You won the bet, you got 5 dollars")
+        print("You now have $%s." % money)
+
 
 print("You did %s rounds." % rounds)
 
 print("Your Money is %s dollars." % money)
+
+print("Your high score was %s rounds" % rounds)
+
+print("Game Over")
