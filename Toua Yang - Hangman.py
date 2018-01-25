0598c38f4600820865de925af270dac7d6b994ad
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-01-25 10:34:11 -0800

    Improvements

--- Toua Yang - Hangman.py
+++ Toua Yang - Hangman.py
@@ -1,5 +1,13 @@
 import random
 
+
+#Hangman
+#1. Make a word bank - 10 items
+#2. Select a random item to guess
+#3. Add a guess to the list of letters guessed
+#4. Reveal letters based on input
+#5. Create win and lose conditions
+
 import string
 
 letters_guessed = []
@@ -8,11 +16,19 @@ guesses = 10
 
 print("Welcome to Hangman!!")
 
-words_bank = ["NANI", "EDISON", "Georgie", "EXIT", "TREE", "TOUA", "HOOD", "WHALE", "EZIO", "BATMAN"]
+words_bank = ["nani", "edison", "georgie", "exit", "tree", "clock", "hood", "whale", "turtle", "batman"]
 
 random_word = random.choice(words_bank)
 
-print(random_word)
+letters = list(string.ascii_lowercase)
 
+while guesses > 0:
+    guesses -= 1
+    print("These are your letters %s" % letters)
+    answers = input("Guess a letter")
 
+    lower = answers.lower()
+    letters_guessed.append(lower)
+    print(lower)
+    if letters_guessed == random_word:
 
