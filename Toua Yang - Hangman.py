8925de89e29912c472cfdbe5175e087b5d82bd65
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-01-29 10:35:45 -0800

    Improvements

--- Toua Yang - Hangman.py
+++ Toua Yang - Hangman.py
@@ -1,3 +1,4 @@
+
 import random
 
 
@@ -25,14 +26,24 @@ letters = list(string.ascii_lowercase)
 print(random_word)
 
 while guesses > 0:
-    guesses -= 1
-    print("These are your letters %s" % letters_guessed)
+    output = []
+    for letter in random_word:
+        if letter in letters_guessed:
+            output.append(letter)
+        else:
+            output.append("*")
+    print(output)
     answers = input("Guess a letter ")
+    if letters_guessed == letter in random_word:
+        guesses += 0
+    else:
+        guesses -= 1
+        print("You got it wrong")
 
     lower = answers.lower()
     letters_guessed.append(lower)
     print("You have %s guesses left" % guesses)
-    if letters == random_word:
+    if answers == random_word:
         print("You win!")
 
 print("Game Over")
