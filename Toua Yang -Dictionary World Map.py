a6942b2985c45ab78b4b668201423b0295278281
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-02-15 10:41:51 -0800

    IMPROVEMENTS

--- Toua Yang -Dictionary World Map.py
+++ Toua Yang -Dictionary World Map.py
@@ -13,7 +13,9 @@ world_map = {
         'NAME': 'Top of Mid',
         'DESCRIPTION': "You are at the Top of Mid",
         'PATHS': {
-            'SOUTH': 'NOXUS'
+            'SOUTH': 'NOXUS',
+            'EAST': 'OUTSIDEOFLONGDOORS',
+            'NORTH': 'MID'
         }
 
     },
@@ -24,12 +26,36 @@ world_map = {
             'NORTH': 'LONGDOORS'
         }
     },
+    'LONGDOORS': {
+        'NAME': 'Long Doors',
+        'DESCRIPTION': "You are in Long Doors, you see 5 boxes on top of each other",
+        'PATHS': {
+            'NORTH': 'LONG',
+            'SOUTH': 'OUTSIDEOFLONGDOORS'
+        }
+    },
     'OUTSIDEOFTUNNELS': {
         'NAME': 'Outside of Tunnels',
         'DESCRIPTION': "You are outside of Tunnels",
         'PATHS': {
-            'NORTH': 'LONGDOORS'
-            'SOUTH': 'OUTSIDEOFLONGDOORS'
+            'NORTH': 'TUNNELS',
+            'SOUTH': 'NOXUS'
+        }
+    },
+    'TUNNELS': {
+        'NAME': 'Tunnels',
+        'DESCRIPTION': "You are inside of Tunnels, you see a staircase leading down from the east, and light from the north",
+        'PATHS': {
+            'NORTH': 'HOWLING MARSH',
+            'SOUTH': 'OUTSIDEOFTUNNELS',
+            'EAST': 'LOWERTUNNELS'
+        }
+    },
+    'HOWLING MARSH': {
+        'NAME': 'Howling Marsh',
+        'DESCRIPTION': "You are in Howling Marsh, the air around you becomes thick and you feel as if someone is watching you... You see a window to the east that is broken but light is coming from it",
+        'PATHS': {
+            'EAST': ' ROADTOIONIA'
         }
     }
 }
