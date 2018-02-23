4f7974797300893e9453eea9f8b83373531e6f71
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-02-23 10:46:11 -0800

    Stuff

---
+++ Toua Yang - Pythagorean.py
@@ -0,0 +1,7 @@
+def pythagorean(a, b):
+    add = a ** 2 + b ** 2
+    answer = add ** 0.5
+    return answer
+
+
+print(pythagorean(5,12))
--- Toua Yang - World Map OP.py
+++ Toua Yang - World Map OP.py
@@ -1,3 +1,19 @@
 class Room(object):
-    def __init__(self, door, description, north, south, east, west, window):
-        self.door
+    def __init__(self, name, north, south, east, west, items, description):
+        self.name = name
+        self.north = north
+        self.west = west
+        self.east = east
+        self.south = south
+        self.items = items
+        self.description = description
+
+    def move(self, direction):
+        global current_node
+        current_node = globals()[getattr(self, direction)]
+
+
+# Intialize Rooms
+west_house = Room("West of House", 'north_house', None)
+east_house = Room("East of House", 'west_house', None)
+north_house = Room("North of House", 'east_house', None)
