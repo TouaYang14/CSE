ab94ce4f08a34c183ca8e1b3dfc5328aa60ca864
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-03-07 10:46:17 -0800

    Updated

---
+++ Toua Yang - Characters.py
@@ -0,0 +1,16 @@
+class Character(object):
+    def __init__(self, name, inventory, abilities, stats, health, description, dialogue, attack):
+        self.name = name
+        self.inventory = inventory
+        self.abilities = abilities
+        self.stats = stats
+        self.health = health
+        self.description = description
+        self.dialogue = dialogue
+        self.attack = attack
+
+
+snapper = Character('snapper', 'wooden_stick', 'water_spiral', 'ATK = 97, DEF = 53', 'HP = 500', "Snapper\n"
+                    " is a turtle that knows martial art and is seeking to find a way to get stronger.", "Snapper\n"
+                    " was born in a small village and seeks a fighting spirit. Ever since he was a small kid he\n"
+                    " has been trying to get stronger. He is seeking to master the water style", 'ATK = 97')
