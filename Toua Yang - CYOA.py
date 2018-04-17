0f1e67ac3668d6e81554bcbea88954c412481b1f
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-04-17 10:05:31 -0700

    Imrpovements

--- Toua Yang - CYOA.py
+++ Toua Yang - CYOA.py
@@ -1,10 +1,12 @@
-#import statements
-#class definitions
-#items
-#characters
-#rooms
-#instantiate classes
-#controller
+# import statements
+# class definitions
+# items
+# characters
+# rooms
+# instantiate classes
+# controller
+
+
 class Item(object):
     def __init__(self, description, name):
         self.equip = False
@@ -18,7 +20,13 @@ class Item(object):
 
 class Weapons(Item):
     def __init__(self, attack, block, name, description):
-        super(Weapons, self).__init__(attack, block)
+        super(Weapons, self).__init__('Tools that you use to do damage', 'Weapons')
+        self.equip = False
+        self.unequip = True
+        self.attack = attack
+        self.block = block
+        self.name = name
+        self.description = description
 
 
 class WoodenSword(Weapons):
@@ -61,39 +69,37 @@ class Equipment(Item):
         self.value = value
 
 
-class DarkArmor(Equipment):
+class Armor(Equipment):
     def __init__(self):
-        super(DarkArmor, self).__init__('Dark Armor for the chest', 'Dark Chestplate, where\n'
+        super(Armor, self).__init__('Armor for the chest', 'Chestplate, where\n'
                                                                     ' only those who have enough potential can use it',
-                                                                        950000000)
+                                                                    500)
 
 
-class DarkLeggings(Equipment):
+class BootsOfSwiftness(Equipment):
     def __init__(self):
-        super(DarkLeggings, self).__init__('Dark Armor for leggings' 'Dark Leggings', 'A dark\n'
-                                                                                      ' armor for the legs', 950000000)
+        super(BootsOfSwiftness, self).__init__('BootsOfSwiftness', 'a boot that makes you go faster', 500)
 
 
-class DarkHelment(Equipment):
+class PitKey(Item):
     def __init__(self):
-        super(DarkHelment, self).__init__('Dark Helment', 'A Dark helment only for those who are are strong enough',
-                                          950000000)
+        super(PitKey, self).__init__('The key that is require to open the door in pit at long', 'PitKey')
 
 
-class DarkGauntlets(Equipment):
+class Gauntlet(Equipment):
     def __init__(self):
-        super(DarkGauntlets, self).__init__('Dark Gauntlets', 'A Gauntlets to which you need to complete the dark set',
-                                            950000000)
+        super(Gauntlet, self).__init__('Gauntlet', 'A gauntlet that is used to wield some objects', 400)
 
 
-class DarkSword(Weapons):
+class SteelSword(Weapons):
     def __init__(self):
-        super(DarkSword, self).__init__(350, 0, 'Dark Sword', 'THE DARK SWORD!!!')
+        super(SteelSword, self).__init__(68, 0, 'Steel Sword', 'A sturdy steel sword')
 
 
-class DarkShield(Weapons):
+class SteelShield(Weapons):
     def __init__(self):
-        super(DarkShield, self).__init__(100, 600, 'Dark Shield', 'THE DARK SHIELD!')
+        super(SteelShield, self).__init__(100, 600, 'Steel Shield', 'A steel\n'
+                                                                    ' shield that is better than the wooden shield')
 
 
 class Character(object):
@@ -110,6 +116,21 @@ class Character(object):
             self.health += 25
 
 
+
+wooden_sword = WoodenSword()
+shield = Shield()
+battle_axe = BattleAxe()
+wings = Wings()
+clock = Clock()
+compass = Compass()
+armor = Armor()
+bootsofswiftness = BootsOfSwiftness()
+pitkey = PitKey()
+gauntlet = Gauntlet()
+steelsword = SteelSword()
+steelshield = SteelShield()
+
+
 Snapper = Character('Snapper', None, 320, 0, "Snapper is a turtle , that is seeking to Ionia\n"
                     " so he can deliver a message to the Master", None)
 
@@ -117,14 +138,13 @@ Villager1 = Character('Tom', None, 200, 0, 'A villager that is wondering around 
 
 
 class Room(object):
-    def __init__(self, name, north, south, east, west, items, item2, description):
+    def __init__(self, name, north, south, east, west, items, description):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
         self.west = west
         self.items = items
-        self.item2 = item2
         self.description = description
 
     def move(self, direction):
@@ -133,41 +153,44 @@ class Room(object):
 
 
 # Intialize Rooms
-noxus = Room('noxus', 'top_mid', None, 'outsideoflongdoors', 'outsideoftunnels', 'Wooden_sword', 'Wooden_shield', '\n'
-             'You are in Noxus, you must find a way to get to Ionia')
-outsideoftunnels = Room('outsideoftunnels', 'tunnels', 'noxus', None, None, None, None, 'You are outside of Tunnels')
-outsideoflongdoors = Room('outsideoflongdoors', 'longdoors', 'noxus', None, 'top_mid', None, None, 'You are outside\n'
+noxus = Room('noxus', 'top_mid', None, 'outsideoflongdoors', 'outsideoftunnels', None, 'You are in Noxus, you must\n'
+                                                                                       ' find a way to get to Ionia')
+outsideoftunnels = Room('outsideoftunnels', 'tunnels', 'noxus', None, None, wooden_sword, 'You are outside of Tunnels')
+outsideoflongdoors = Room('outsideoflongdoors', 'longdoors', 'noxus', None, 'top_mid', None, 'You are outside\n'
                           ' of Long Doors')
-longdoors = Room('longdoors', None, 'outsideoflongdoors', 'long', None, 'Smoke_Capsule', None, 'You are in\n'
+longdoors = Room('longdoors', None, 'outsideoflongdoors', 'long', None, armor, 'You are in\n'
                  ' Long Doors, you see 5 boxes on top of each other')
-long = Room('long', 'icathia', 'outsideoflongdoors', None, None, None, None, 'You are at Long, you see a mist of\n'
+long = Room('long', 'icathia', None, None, 'longdoors', None, 'You are at Long, you see a mist of\n'
             ' clouds north.')
-tunnels = Room('tunnels', 'howling_marsh', 'outsideoftunnels', 'lower_tunnels', None, 'Battle_Axe', '\n'
-               'small_health_potion', 'You are inside\n'
-               ' of Tunnels, you see a staircase leading down from the east, and light from the north')
-lower_tunnels = Room('lowertunnels', None, None, 'mid', 'tunnels', 'Clock', 'Compass', 'You are in lower tunnels, you\n'
-                    'see a clock and a compass on the table')
-mid = Room('mid', 'mid_doors', 'top_mid', 'staircase', 'lower_tunnels', None, None, 'You are at mid and you see a door\n'
-           'north, you see a hallway leading to a stair to the east.')
-staircase = Room('staircase', 'shadow_isle', 'top_mid', None, 'mid', None, None, 'You see a hallway\n'
+tunnels = Room('tunnels', 'howling_marsh', 'outsideoftunnels', 'lower_tunnels', None, battle_axe, 'You are inside\n'
+               ' of Tunnels, and you see an axe, you see a staircase leading down from the\n'
+               ' east, and light from the north')
+lower_tunnels = Room('lower_tunnels', None, None, 'mid', 'tunnels', [clock, compass], 'You are in lower\n'
+                     ' tunnels, you see a clock and a compass on the table')
+mid = Room('mid', 'mid_doors', 'top_mid', 'staircase', 'lower_tunnels', None, 'You are at mid and you see a\n'
+           'door to the west and a door to the north.')
+pit = Room('pit', 'long', 'pitdoor', 'longdoors', None, None, 'The pit.. There is a door to the south')
+pitdoor = Room('pitdoor', 'pit', None, None, None, wings, 'Where the wings lies. You see the wing on the ground')
+staircase = Room('staircase', 'shadow_isle', 'top_mid', None, 'mid', None, 'You see a hallway\n'
                  ' turning right, and it is\n'
-                 ' leading to a stair case, with dark and misty clouds. As your feet tremble you hear a sound.')
-shadow_isle = Room('shadow_isle', None, 'staircase', 'icathia', None, 'shadow_hammer', None, 'You\n'
+               ' leading to a stair case, with dark and misty clouds. As your feet tremble you hear a sound.')
+shadow_isle = Room('shadow_isle', None, 'staircase', 'icathia', None, gauntlet, 'You\n'
                    ' are finally in the Shadow Isle and there are thick clouds. You feel\n'
-                   ' as if someone is watching you, and you see a hammer laying on the ground.')
-icathia = Room('icathia', 'goose', 'long', None, 'shadow_isle', None, None, 'You are in Icathia, this City\n'
+                   ' as if someone is watching you, and you see a steel shield laying on the ground.')
+icathia = Room('icathia', 'goose', 'long', None, 'shadow_isle', bootsofswiftness, 'You are in Icathia, this City\n'
                ' is close to Ionia')
-ionia = Room('ionia', None, None, 'shadow_isle', 'mid_doors', None, None, 'You have finally reached Ionia, and people\n'
+ionia = Room('ionia', None, None, None, 'mid_doors', None, 'You have finally reached Ionia, and people\n'
              'welcome you to the City.')
-goose = Room('goose', None, 'icathia', None, None, 'egg', None, 'You are in the ally\n'
-             ' of goose, it is a small ally wih a small, you see an egg lying on the ground.')
-howling_marsh = Room('howling_marsh', None, 'tunnels', 'mid_doors', None, None, None, 'You are in Howling\n'
+goose = Room('goose', None, 'icathia', None, None, steelsword, 'You are in the ally\n'
+             ' of goose, it is a small ally wih a small, you see a shield lying on the ground.')
+howling_marsh = Room('howling_marsh', None, 'tunnels', 'mid_doors', None, steelshield, 'You are in Howling\n'
                      ' Marsh.... You feel as very unsafe and see dead trees everywhere.')
-top_mid = Room('top_mid', 'mid', 'noxus', 'outsideoflongdoors', None, None, None, "You\n'"
+top_mid = Room('top_mid', 'mid', 'noxus', 'outsideoflongdoors', None, None, "You\n'"
                "are at the top of mid. You don't see\n"
                " anything particular that is interesting at all. You see a ramp going down to the north.")
-mid_doors = Room('mid_doors', None, 'mid', 'ionia', 'howling_marsh', None, None, 'You\n'
-                 ' are at mid doors, and to your west you see a shadowing place, and to your right is ionia.')
+mid_doors = Room('mid_doors', None, 'mid', 'ionia', 'howling_marsh', None,'You are at mid doors, and to your west you\n'
+                 ' see a shadowing place, and to your right is ionia.')
+
 
 current_node = noxus
 directions = ['north', 'south', 'east', 'west']
