5308b671163480e57a3439f2fe6ec7694b187322
TouaYang14 <31044642+TouaYang14@users.noreply.github.com>
2018-05-18 11:33:52 -0700

    Atack improving

--- Toua Yang - CYOA.py
+++ Toua Yang - CYOA.py
@@ -7,16 +7,14 @@
 # controller
 
 
+print("__          ________ _      _____ ____  __  __ ______ \n"
+" \ \        / /  ____| |    / ____/ __ \|  \/  |  ____|\n"
+"  \ \  /\  / /| |__  | |   | |   | |  | | \  / | |__   \n"
+"   \ \/  \/ / |  __| | |   | |   | |  | | |\/| |  __|  \n"
+"    \  /\  /  | |____| |___| |___| |__| | |  | | |____ \n"
+"     \/  \/   |______|______\_____\____/|_|  |_|______|")
 
-# print("__          ________ _      _____ ____  __  __ ______ \n"
-#  "\ \        / /  ____| |    / ____/ __ \|  \/  |  ____|\n"
-#   "\ \  /\  / /| |__  | |   | |   | |  | | \  / | |__   \n"
-#    "\ \/  \/ / |  __| | |   | |   | |  | | |\/| |  __|  \n"
-#     "\  /\  /  | |____| |___| |___| |__| | |  | | |____ \n"
-#      "\/  \/   |______|______\_____\____/|_|  |_|______|"
 
-
-")
 class Item(object):
     def __init__(self, name, description):
         self.equip = False
@@ -26,34 +24,34 @@ class Item(object):
 
 
 class Weapons(Item):
-    def __init__(self, attack, block, name, description):
+    def __init__(self, damage, block, name, description):
         super(Weapons, self).__init__('Tools that you use to do damage', 'Weapons')
         self.equip = False
         self.unequip = True
-        self.attack = attack
+        self.damage = damage
         self.block = block
         self.name = name
         self.description = description
 
 
 class Dagger(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(Dagger, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(Dagger, self).__init__(damage, block, name, description)
 
 
 class WoodenSword(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(WoodenSword, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(WoodenSword, self).__init__(damage, block, name, description)
 
 
 class Shield(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(Shield, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(Shield, self).__init__(damage, block, name, description)
 
 
 class BattleAxe(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(BattleAxe, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(BattleAxe, self).__init__(damage, block, name, description)
 
 
 class Wings(Item):
@@ -88,10 +86,9 @@ class Chestplatearmor(Equipment):
         self.defense = defense
 
 
-class BootsOfSwiftness(Equipment):
-    def __init__(self, name, description, value, defense=0):
-        super(BootsOfSwiftness, self).__init__(name, defense, description, value)
-        self.defense = defense
+class Rock(Item):
+     def __init__(self, name, description):
+        super(Rock, self).__init__(name, description)
 
 
 class PitKey(Item):
@@ -106,25 +103,29 @@ class Gauntlet(Equipment):
 
 
 class SteelSword(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(SteelSword, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(SteelSword, self).__init__(damage, block, name, description)
 
 
 class SteelShield(Weapons):
-    def __init__(self, attack, block, name, description):
-        super(SteelShield, self).__init__(attack, block, name, description)
+    def __init__(self, damage, block, name, description):
+        super(SteelShield, self).__init__(damage, block, name, description)
 
 
 class Character(object):
-    def __init__(self, name, health, armor, attack, description, dialogue):
+    def __init__(self, name, health, armor, damage, description, dialogue):
         self.name = name
         self.health = health
         self.armor = armor
-        self.attack = attack
+        self.damage = damage
         self.description = description
         self.dialogue = dialogue
         self.inventory = []
 
+    def take_damage(self, amount):
+        self.health -= amount
+        if self.health < 0:
+            self.health = 0
 
 wooden_sword = WoodenSword(102, 0, 'Wooden Sword', 'A wooden sword')
 shield = Shield(0, 41, 'Wooden Shield', 'a wooden shield')
@@ -132,8 +133,8 @@ battle_axe = BattleAxe(76, 0, 'Battle Axe', 'A powerful battle axe that was used
 wings = Wings('Wings', 'wings that can make you fly')
 clock = Clock('Clock', 'a clock that tells you the time')
 compass = Compass('Compass', 'A compass that tells you what direction you are going')
-chestplatearmor = Chestplatearmor('Chestplatearmor', 'a basic armor that will protect you from attacks', 100, 50)
-bootsofswiftness = BootsOfSwiftness('Boots of swiftness', 'Boots that make you go faster', 100, 30)
+chestplatearmor = Chestplatearmor('Chestplatearmor', 'a basic armor that will protect you from damage', 100, 50)
+rock = Rock('Rock', 'A rock that is used for nothing')
 pitkey = PitKey('Pitkey', 'The key that is required to unlock the door in pit')
 gauntlet = Gauntlet('Gauntlet', 'Gauntlets that protect nope', 100, 30)
 steelsword = SteelSword(205, 0, 'Steel Sword', 'A steel sword that is stronger than a wooden_sword')
@@ -179,7 +180,7 @@ long = Room('long', 'icathia', 'pit', None, 'longdoors', None, 'You are at Long,
             ' clouds north.', None)
 tunnels = Room('tunnels', 'howling_marsh', 'outsideoftunnels', 'lower_tunnels', None, battle_axe, 'You are inside\n'
                ' of Tunnels, and you see an axe, you see a staircase leading down from the\n'
-               ' east, and light from the north', [Enemy1])
+               ' east, and light from the north', Enemy1)
 lower_tunnels = Room('lower_tunnels', None, None, 'mid', 'tunnels', compass, 'You are in lower\n'
                      ' tunnels, you see a compass on the table', None)
 mid = Room('mid', 'mid_doors', 'top_mid', 'staircase', 'lower_tunnels', None, 'You are at mid and you see a\n'
@@ -193,7 +194,7 @@ staircase = Room('staircase', 'shadow_isle', 'top_mid', None, 'mid', None, 'You 
 shadow_isle = Room('shadow_isle', None, 'staircase', 'icathia', None, gauntlet, 'You\n'
                    ' are finally in the Shadow Isle and there are thick clouds. You feel\n'
                    ' as if someone is watching you, and you see a steel shield laying on the ground.', None)
-icathia = Room('icathia', 'goose', 'long', None, 'shadow_isle', bootsofswiftness, 'You are in Icathia, this City\n'
+icathia = Room('icathia', 'goose', 'long', None, 'shadow_isle', Rock, 'You are in Icathia, this City\n'
                ' is close to Ionia', None)
 ionia = Room('ionia', None, None, None, 'mid_doors', None, 'You have finally reached Ionia, and people\n'
              'welcome you to the City.', None)
@@ -222,15 +223,18 @@ while True:
         print("Do you wanna pick up item?")
         print(current_node.items.name)
 
+    if current_node.enemies is not None:
+        print("There is an Enemy here. IT'S THE %s" % current_node.enemies.name)
+        print("What do you want to do?")
+        if command = 'fight':
+
+
     command = input('>_').lower()
 
     if command == 'yes' and current_node.items is not None:
         print('You took %s.' % current_node.items.name)
         Snapper.inventory.append(current_node.items)
         current_node.items = None
-
-    if command == 'use':
-
 
     if command == 'quit':
         quit(0)
